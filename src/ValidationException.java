eaa979d7db9ebd610f43e21b70af49d78f107b1e
Aaron Averill <aaron@zonefivesoftware.com>
2012-11-25 17:36:09 -0500

    ValidateionException class

---
+++ src/ValidationException.java
@@ -0,0 +1,22 @@
+/*****************************************************************************************
+ * 
+ * ValidationException.java
+ *  
+ * Aaron Averill
+ * John Lloyd
+ * Matthew Marum
+ * Rob Parsons
+ * 
+ * CSC 540
+ * Fall 2012
+ * 
+ * ValidationException.java is an error class (duh).
+ * 
+ */
+
+
+public class ValidationException extends Exception {
+  public ValidationException(String msg) {
+    super(msg);
+  }
+}
